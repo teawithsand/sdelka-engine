42efa8d6cae141265d5babdc231f37caff69ddd1
teawithsand <teawithsand@gmail.com>
2023-03-12 17:03:53 +0000

    Fixed dexie during testing in jest

--- jest.config.ts
+++ jest.config.ts
@@ -88,7 +88,9 @@ export default {
 	// ],
 
 	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+	moduleNameMapper: {
+		'^dexie$': require.resolve('dexie'),
+	},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	modulePathIgnorePatterns: ["<rootDir>/dist/", "node_modules"],
