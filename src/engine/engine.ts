3ba04844c386ad2fe0f95ed78d74a0e37ae167a4
teawithsand <teawithsand@gmail.com>
2023-05-01 10:59:44 +0000

    Added interface types for engine with cards managed externally

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,8 +1,36 @@
+import { Cursor } from "../pubutil"
+import { SyncRequest } from "../util/sync"
+
+/**
+ * Engine, which like SM2 engine uses key/value+features DB to run whole learning process.
+ * 
+ * Such databases are easy to synchronize between devices and synchronization is the main purpose of existence
+ * of this interface.
+ */
+export interface CardDataBasedEngineManagement<T> {
+	getCardData: (id: string) => Promise<T | null>
+	setCardData: (id: string, data: T) => Promise<void>
+    hasCardData: (id: string) => Promise<boolean>
+
+	getEntriesForSyncRequest: (req: SyncRequest) => Cursor<T>
+}
+
+/**
+ * Engine, which has cards manually deleted/added to it.
+ */
+export interface EngineCardManagement {
+    hasCard: (id: string) => Promise<boolean>
+	addCard: (id: string) => Promise<void>
+	deleteCard: (id: string) => Promise<void>
+}
+
 /**
  * Abstract type representing any engine.
+ *
+ * TODO(teawithsand): replace parameter T with string
  */
-export interface Engine<C, A, S> {
-    getCurrentCard: () => Promise<C | null>
-    answer: (answer: A) => Promise<void>
-    getStats: () => Promise<S>
+export interface Engine<T, A, S> {
+	getCurrentCard: () => Promise<T | null>
+	answer: (answer: A) => Promise<void>
+	getStats: () => Promise<S>
 }
